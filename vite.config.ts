2af129cc1b38acf60b1a6dc29c62bc287e81542d
JiskaBeekman <jbeekman@prismanet.nl>
2025-05-16 18:17:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/OC-beslisboom/',   // hier je repo naam als subpad
+  base: '/OC-beslisboom/',  
   plugins: [react()]
 })
